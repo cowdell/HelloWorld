3be63949d1ba85cbc16279e0fe41e73853c22c36
cowdell <cajcowdell@hotmail.com>
2013-10-14 22:31:56 -0600

    FIRST COMMIT

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ccowdell
+ * Date: 10/14/13
+ * Time: 10:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
